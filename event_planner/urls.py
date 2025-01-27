66b1ee1ffbc1cec15b99ec2b274955f3af207668
Mark Mcloughlin <mark25mky@gmail.com>
2025-01-27 20:40:48 +0000

    added login, logged out and base and populated login with placeholder

--- event_planner/urls.py
+++ event_planner/urls.py
@@ -19,7 +19,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('planner_pro.urls'))
+    path('', include('planner_pro.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
