f4de5b558e51ff7cdbde972010dbf13edcf7921f
Mark Mcloughlin <mark25mky@gmail.com>
2025-01-30 17:51:56 +0000

    updated models

--- planner_pro/models.py
+++ planner_pro/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Event(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, default="Untitled Event")
     description = models.TextField()
     date = models.DateTimeField()
     location = models.CharField(max_length=255, default="Unknown Location")
