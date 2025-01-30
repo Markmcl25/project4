fa26602ece0447e630b4a5e9ae38b4d72226bc7d
Mark Mcloughlin <mark25mky@gmail.com>
2025-01-30 16:00:31 +0000

    updated views to prevent logged out users from accessing dash

--- planner_pro/views.py
+++ planner_pro/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Event
 from django.utils.timezone import now
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     upcoming_events = Event.objects.filter(date__gte=now()).order_by('date')[:5]  # Next 5 events
@@ -15,3 +16,7 @@ def event_list(request):
 
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')    
