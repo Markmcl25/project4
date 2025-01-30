18d504dbe44617d1604bab2212f70337b7fec380
Mark Mcloughlin <mark25mky@gmail.com>
2025-01-30 16:59:50 +0000

    updated url path for sign up

--- planner_pro/urls.py
+++ planner_pro/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import signup
 
 urlpatterns = [
+    path("signup/", signup, name="signup"),
     path('', views.home, name='home'),
     path('create/', views.create_event, name='create_event'),
     path('events/', views.event_list, name='event_list'),
