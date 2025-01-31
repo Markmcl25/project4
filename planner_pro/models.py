4a0aa6e79b4314de88111c42c40ff0edf9b7d484
Mark Mcloughlin <mark25mky@gmail.com>
2025-01-31 16:57:52 +0000

    added change to category in models

--- planner_pro/models.py
+++ planner_pro/models.py
@@ -6,6 +6,7 @@ class Event(models.Model):
     date = models.DateTimeField()
     location = models.CharField(max_length=255, default="Unknown Location")
     image = models.ImageField(upload_to="event_images/", blank=True, null=True)
+    category = models.CharField(max_length=100, default="General")
 
     def __str__(self):
         return self.title
