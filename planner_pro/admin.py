052e39255a184cd290b8b4a710f8fedc698329db
Mark Mcloughlin <mark25mky@gmail.com>
2025-02-03 18:35:00 +0000

    updated admin to include event admin

--- planner_pro/admin.py
+++ planner_pro/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'location', 'category', 'creator', 'is_published')
+    list_filter = ('date', 'category', 'is_published')
+    search_fields = ('title', 'location', 'description')
+    ordering = ('-date',)
+    list_editable = ('is_published',)
+
+    # To ensure the creator is automatically assigned
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:  # Only set creator when creating the event
+            obj.creator = request.user
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Event, EventAdmin)
