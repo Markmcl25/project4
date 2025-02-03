be67a7d8a9354d27285da1799dffddc82f644a64
Mark Mcloughlin <mark25mky@gmail.com>
2025-02-03 18:49:15 +0000

    updated model to inclide admin toggling

--- planner_pro/models.py
+++ planner_pro/models.py
@@ -1,12 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Event(models.Model):
-    title = models.CharField(max_length=255, default="Untitled Event")
-    description = models.TextField()
+    title = models.CharField(max_length=200)
     date = models.DateTimeField()
-    location = models.CharField(max_length=255, default="Unknown Location")
-    image = models.ImageField(upload_to="event_images/", blank=True, null=True)
-    category = models.CharField(max_length=100, default="General")
+    location = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='event_images/', blank=True, null=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_published = models.BooleanField(default=True)  # For admin toggling
 
     def __str__(self):
         return self.title
